d2ee1bd6a7d16edd62d71e7783c72ee45ccf1426
Sumit <sumitrs2711@gmail.com>
2025-10-06 10:50:35 +0530

    Add MDX support to Vite configuration by updating alias extensions

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".mdx"], // add mdx support
     },
   },
 })
